5ff73a0cdd3b2a2b154e7c80016e55500b295005
Bruno Ferreira <120688546+brunosanfe@users.noreply.github.com>
2024-01-27 19:23:50 -0300

    Create lacos.py

---
+++ lacos.py
@@ -0,0 +1,20 @@
+for i in range (1,21):
+  if (i==13):
+    continue
+  else
+    print(i)
+
+cont = 1
+while(cont<=20):
+  if (cont == 13):
+    cont+=
+    continue
+  else:
+    print(contador)
+    cont+=
+
+for i in range (20, 0, -1):
+  if (i==13):
+    continue
+  else:
+    print (i)
